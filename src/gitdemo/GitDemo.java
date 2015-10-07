b83c3f552abc7b7b54bb1c22dc5e6eb8ed9c9883
Jonh Doe <johndoe2example.com>
2015-10-07 10:51:29 -0300

    creation d'un depot

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -6,7 +6,7 @@ public class GitDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bonjour John");
     }
     
 }
